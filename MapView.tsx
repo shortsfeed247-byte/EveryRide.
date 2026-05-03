b8209ccd3ae2fe6862ca0e1a17d4ea14be267b91
shortsfeed247-byte <shortsfeed24.7@gmail.com>
2026-05-03 20:26:31 +0530

    Add MapView component with embedded Google Map

---
+++ MapView.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function MapView() {
+  return (
+    <iframe
+      src="https://maps.google.com/maps?q=28.6139,77.2090&z=15&output=embed"
+      width="100%"
+      height="100%"
+      style={{ border: 0 }}
+    />
+  );
+}
