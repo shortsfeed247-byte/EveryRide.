9e0ddb374050cec4c0b8e0103915b92c7b64b1fe
shortsfeed247-byte <shortsfeed24.7@gmail.com>
2026-05-03 20:08:27 +0530

    Add MapComponent to display Google Maps

---
+++ MapComponent.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function MapComponent() {
+  return (
+    <iframe
+      width="100%"
+      height="100%"
+      src="https://maps.google.com/maps?q=28.6139,77.2090&z=15&output=embed"
+    />
+  );
+}
